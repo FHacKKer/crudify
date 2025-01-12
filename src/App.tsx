b44f852db6c49fc520a791e2db8a8d3be740a9f8
FHACKKER <ranjhaplaysyt@gmail.com>
2025-01-12 15:06:29 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className={`h-screen w-full flex flex-col items-center justify-around`}>
+      <h1 className={`text-4xl`}>Welcome to Crudify</h1>
+    </div>
+  )
+}
+
+export default App
